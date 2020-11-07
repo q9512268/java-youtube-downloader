29a390ce07ef26476917e08784cb39aebededec3
q9512268 <chrstn.lttck@icloud.com>
2020-11-07 11:53:50 +0100

    ...

--- src/test/java/com/github/kiulian/downloader/ManipulateVideoTests.java
+++ src/test/java/com/github/kiulian/downloader/ManipulateVideoTests.java
@@ -4,13 +4,40 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import utils.files.FileUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class ManipulateVideoTests {
 
 
     @Test
-    @DisplayName("download video sync should be successful")
     void splitVideo_Success() {
 
-        String inputPath = FileUtils.getResourcesPath() + "/videos/";
+        try {
+            String inputPath = FileUtils.getResourcesPath() + "/videos/zoo.mp4";
+            File file = new File(inputPath);
+            InputStream inputStream = new FileInputStream(file);
+
+            inputStream.read()
+
+            /**
+             * InputStream inputstream = new FileInputStream("c:\\data\\input-text.txt");
+             *
+             * int data = inputstream.read();
+             * while(data != -1) {
+             *   //do something with data...
+             *   doSomethingWithData(data);
+             *
+             *   data = inputstream.read();
+             * }
+             * inputstream.close();
+             */
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
